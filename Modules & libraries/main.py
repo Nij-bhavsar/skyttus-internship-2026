9cebe6d138f8db1d9bed32306f00d8a81ef3121e
Nij D. Bhavsar <nijbhavsar4433@gmail.com>
2026-01-21 10:46:03 +0530

    Add files via upload

---
+++ Modules & libraries/main.py
@@ -0,0 +1,27 @@
+# 1
+import math_nb
+print("Addition:",math_nb.add(23,10))
+print("Subtraction:",math_nb.sub(23,5))
+print("Multipication:",math_nb.mul(10,5))
+print("Division:",math_nb.div(10,23))
+
+# 2
+import math_nb
+string="Nij Bhavsar"
+print("Upper:",math_nb.to_upper(string))
+print("Lower:",math_nb.to_lower(string))
+print("Reverse_string:",math_nb.reverse_string(string))
+print("Count_Vowel:",math_nb.count_vowel(string))
+print("Is_Palindrome:",math_nb.is_palindrom(string))
+# 6
+import math_nb
+print("Circle_area:",math_nb.area(6))
+print("Circumference:",math_nb.circumference(4))
+print("Rectangle:",math_nb.area1(9,8))
+print("Perimeter:",math_nb.parimeter(5,5))
+# 7
+import math_nb
+print("Addition:",math_nb.add(33,90))
+print("Subtraction:",math_nb.sub(7,18))
+print("Multipication:",math_nb.mul(33,33))
+print("Division:",math_nb.div(33,5))
