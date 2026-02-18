c994c2c63ffeac685b682726c66b6c3a11fe0fb0
Nij D. Bhavsar <nijbhavsar4433@gmail.com>
2026-02-18 13:23:05 +0530

    Add files via upload

---
+++ React/assesment_1/src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import About from "../components/About";
+import Footer from "../components/Footer";
+
+function AboutPage() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Footer />
+    </>
+  );
+}
+
+export default AboutPage;
